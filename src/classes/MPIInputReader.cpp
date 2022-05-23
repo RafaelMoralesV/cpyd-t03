3cc4ce2470e0776682d920a36bb8421cf0154318
Rafael Morales <rmorales@utem.cl>
2022-05-22 22:53:40 -0400

    No funca

--- src/classes/MPIInputReader.cpp
+++ src/classes/MPIInputReader.cpp
@@ -6,12 +6,13 @@
 
 namespace cpyd {
     MPIInputReader::MPIInputReader(std::string & input, std::string & output)
-        : InputReader(input, output), m_input(input), m_output(output){ }
+        : InputReader(input, output), m_input(input), m_output(output){
+        csvFile.close();
+    }
 
     void MPIInputReader::readFile() {
         MPI_File in;
         int rank, size;
-        int ierr;
 
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
         MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -30,14 +31,51 @@ namespace cpyd {
 
         std::cout << "MPI: Rank " << rank << " has " << ndata << " characters." << std::endl;
 
+        std::stringstream s(data), out, fault_string;
+        std::string row;
+        if(rank == 0) std::getline(s, row);
+        while(std::getline(s, row)){
+            std::string word;
+            std::stringstream rowstream(row);
+
+            if(row.length() < 86){
+                if(rank == 0) {
+                    fault_string << row;
+                    continue;
+                }
+                // La linea es menor a lo esperado.
+                MPI_Send(&row, (int) row.length() + 1, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+                continue;
+            }
+
+            out << processRow(row) << std::endl;
+        }
+
+
+        if(rank != 0) {
+            std::string out_str = out.str();
+            MPI_Send(&out_str, (int) out_str.length(), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+        }
+        else {
+            std::string out_str;
+            output << out.str();
+            for(int source = 1; source < size; source++) {
+                MPI_Status status;
+                MPI_Recv(&out_str, ndata, MPI_CHAR, source, 0, MPI_COMM_WORLD, &status);
+
+                std::cout << "SOURCE " << source << ": " << out_str << std::endl;
+
+                output << out_str;
+            }
+        }
+
         delete [] data;
 
         MPI_File_close(&in);
         MPI_Info_free(&info);
     }
 
-    void partitionFile(const int filesize, const int rank, const int size,
-                       const int overlap, int *start, int *end) {
+    void partitionFile(const int filesize, const int rank, const int size, const int overlap, int *start, int *end) {
         int localsize = filesize/size;
         *start = rank * localsize;
         *end   = *start + localsize-1;
@@ -46,8 +84,7 @@ namespace cpyd {
         if (rank != size-1) *end   += overlap;
     }
 
-    void readdataMPI(MPI_File *in, const int rank, const int size, const int overlap,
-                     char **data, int *ndata) {
+    void readdataMPI(MPI_File *in, const int rank, const int size, const int overlap, char **data, int *ndata) {
         MPI_Offset filesize;
         int start;
         int end;
@@ -64,7 +101,7 @@ namespace cpyd {
 
         // everyone reads in their part
         MPI_File_read_at_all(*in, (MPI_Offset)start, *data,
-                             (MPI_Offset)(*ndata), MPI_CHAR, MPI_STATUS_IGNORE);
+                             (int)(MPI_Offset)(*ndata), MPI_CHAR, MPI_STATUS_IGNORE);
         (*data)[*ndata] = '\0';
     }
 } // cpyd
