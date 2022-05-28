bbfaffa2241d2e41685c70393df8f94cbacb4fa4
Rafael Morales <rmorales@utem.cl>
2022-05-27 20:08:51 -0400

    Arreglado argumentos del programa

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,65 @@
 
 using namespace cpyd;
 
+struct config {
+    std::string input;
+    std::string output;
+    std::string mode;
+};
+
+config getConfig(std::vector<std::string> args){
+    config conf;
+    std::stringstream excep_message;
+
+    // Se encontro "--help" entre los argumentos.
+    if(std::find(args.begin(), args.end(), "--help") != args.end()){
+        if(args.size() > 1) {
+            excep_message   << "--help evitará el resto de la ejecución."
+                        << " No lo incluyas con el resto de argumentos!"
+                        << std::endl;
+        }
+
+        excep_message << "El programa requiere 2 argumentos posicionales: " << std::endl
+                  << "<file>, que es la ruta de un archivo .csv de entrada" << std::endl
+                  << "<output>, que es la ruta de un archivo .csv de salida" << std::endl << std::endl
+
+                  << "Adicionalmente hay un argumento opcional, que adopta uno de tres valores: " << std::endl
+                  << "--modo [secuencial|openmp|mpi] (Default: secuencial)" << std::endl
+                  << "\tCada uno de estos modos altera la forma en que se comporta la ejecucion del codigo." << std::endl
+                  << "\tPor defecto, este programa ejecuta el modo secuencial." << std::endl << std::endl
+
+                  << "Ejemplo: test-check ./pruebas.csv ./output.csv --modo openmp" << std::endl;
+
+        throw std::invalid_argument(excep_message.str());
+    }
+
+    // No se han enviado argumentos.
+    if(args.size() < 2){
+        excep_message   << "ERROR: Muy pocos parametros."                   << std::endl
+                        << "Utilización: ./test-check <file> <output>"      << std::endl
+                        << "Utiliza --help para más información."           << std::endl;
+
+        throw std::invalid_argument(excep_message.str());
+    }
+
+    conf.input = args[0];
+    conf.output = args[1];
+
+    if(conf.input.compare(conf.input.length() - 4, 4, ".csv") != 0) {
+        excep_message << "El archivo de INPUT debe terminar en '.csv'!" << std::endl;
+
+        throw std::invalid_argument(excep_message.str());
+    }
+
+    if(conf.output.compare(conf.output.length() - 4, 4, ".csv") != 0) {
+        excep_message << "El archivo de OUTPUT debe terminar en '.csv'!" << std::endl;
+
+        throw std::invalid_argument(excep_message.str());
+    }
+
+    return conf;
+}
+
 int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
     int rank, size;
@@ -13,95 +72,49 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if(rank == 0){
-        std::cout << "Integrantes: " << std::endl
-                  << "\tRafael Morales Venegas" << std::endl
-                  << "\tDiego Oyarce Trejo" << std::endl;
-    }
-
-    // No arguments prompt
-    if(argc == 1 && rank == 0){
-        std::cout << "Utilización: ./test-check --file ./file.csv --out output.csv" << std::endl;
-        std::cout << "Puedes utilizar --help para más información." << std::endl;
-
-        MPI_Finalize();
-        return 0;
-    }
-
     std::vector<std::string> args;
-
     for(int i = 1; i < argc; i++){
         std::string str(argv[i]);
         args.push_back(str);
     }
 
-    // Help message
-    if(std::find(args.begin(), args.end(), "--help") != args.end() && rank == 0){
-        if(argc > 2) {
-            std::cout   << "--help evitará el resto de la ejecución."
-                        << " No lo incluyas con el resto de argumentos!"
-                        << std::endl;
-        }
+    std::cout << "Integrantes: " << std::endl
+              << "\tRafael Morales Venegas" << std::endl
+              << "\tDiego Oyarce Trejo" << std::endl;
 
-        std::cout << "El programa requiere 2 argumentos posicionales: " << std::endl
-            << "--file, que debe ser seguido por la ruta relativa a este programa." << std::endl
-            << "\te.j: --file ./pruebas.csv" << std::endl << std::endl
-            << "--out, seguido por el archivo de output a utilizar. Por defecto, el programa creara un " << std::endl
-            << "\tarchivo llamado 'output.csv'." << std::endl << std::endl
-            << "--modo [secuencial|openmp|mpi] (Default: secuencial)" << std::endl
-            << "\tCada uno de estos modos altera la forma en que se comporta la ejecucion del codigo." << std::endl
-            << "\tPor defecto, este programa ejecuta el modo secuencial." << std::endl;
+    config conf;
 
-        MPI_Finalize();
-        return 0;
-    }
-
-    // Input file
-    auto file_index = std::find(args.begin(), args.end(), "--file");
-    if((file_index == args.end() || argc < 3) && rank == 0) {
-        std::cout << "--file necesita ser seguido por el nombre del archivo .csv a revisar." << std::endl;
+    // Consigo Input y Output
+    try{ conf = getConfig(args); }
+    catch (std::invalid_argument & e) {
+        if(rank == 0) std::cout << e.what();
 
         MPI_Finalize();
         return 1;
     }
 
-    std::string filename = file_index[1];
-    if(filename.compare(filename.length() - 4, 4, ".csv") != 0 && rank == 0) {
-        std::cout << "El archivo de INPUT debe terminar en '.csv'!" << std::endl;
 
-        MPI_Finalize();
-        return 1;
-    }
-
-    // Output file
-    auto out_index = std::find(args.begin(), args.end(), "--out");
-    std::string output_filename = out_index == args.end() ? "output.csv" : out_index[1];
-
-    if(output_filename.compare(output_filename.length() - 4, 4, ".csv") != 0 && rank == 0) {
-        std::cout << "El archivo de OUTPUT debe terminar en '.csv'!" << std::endl;
-
-        MPI_Finalize();
-        return 1;
-    }
+    InputReader* reader;
 
     // Selected mode
     auto mode_index = std::find(args.begin(), args.end(), "--modo");
     std::string mode = mode_index == args.end() ? "secuencial" : mode_index[1];
 
-    InputReader* reader;
     std::vector<std::string> modos {"secuencial", "openmp", "mpi"};
     auto selected = std::find(modos.begin(), modos.end(), mode);
 
     switch (selected - modos.begin()) {
         case 0:
-            reader = new SequentialInputReader(filename, output_filename);
+            if(rank != 0) break;
+            reader = new SequentialInputReader(conf.input, conf.output);
             break;
         case 1:
-            reader = new OpenMPInputReader(filename, output_filename);
+            if(rank != 0) break;
+            reader = new OpenMPInputReader(conf.input, conf.output);
             std::cout << "Selected: OpenMP" << std::endl;
             break;
         case 2:
-            reader = new MPIInputReader(filename, output_filename);
+            reader = new MPIInputReader(conf.input, conf.output);
             if (rank == 0) std::cout << "Selected: MPI" << std::endl;
             break;
         default:
